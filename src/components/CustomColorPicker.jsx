277b9b1e424535b5ea3d840977f6a9cc4f3b6d84
Leyvon <semen_amelchenko@mail.ru>
2023-01-29 17:12:26 +0700

    feat: swatch-color-sq without func; add color-list in utils

--- src/components/CustomColorPicker.jsx
+++ src/components/CustomColorPicker.jsx
@@ -1,11 +1,16 @@
 import React from "react";
 import { CustomPicker } from "react-color";
 import "./CustomColorPicker.css";
+import SwatchColorSquares from "./SwatchColorSquares";
+import { colorList } from "../utils/colorList";
 
 const CustomColorPicker = () => {
   return (
     <div className="color-picker">
-      <div>container-swatch-square</div>
+      <SwatchColorSquares
+        colorList={colorList}
+        onClick={(color) => console.log(`${color} from SwatchColorSquares`)}
+      />
 
       <div className="line"></div>
 
