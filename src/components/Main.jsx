90b56f1a0da70c57177157ef87cc1fcb48719b33
Leyvon <semen_amelchenko@mail.ru>
2023-01-30 01:27:49 +0700

    feat: add functionality to Main

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import "./Main.css";
 
 const tinycolor = require("tinycolor2");
@@ -13,78 +13,20 @@ const CustomPointer = () => {
 };
 
 const Main = ({ hexCode, onChange }) => {
-  const [hsl, setHsl] = useState({
-    h: 0,
-    s: 0,
-    l: 0,
-    a: 0,
-  });
-  const [prevHsl, setPrevHsl] = useState({
-    h: 0,
-    s: 0,
-    l: 0,
-    a: 0,
-  });
-  const [hsv, setHsv] = useState({
-    h: 0,
-    s: 0,
-    v: 0,
-    a: 0,
-  });
-  const [prevHsv, setPrevHsv] = useState({
-    h: 0,
-    s: 0,
-    v: 0,
-    a: 0,
-  });
-  const [hex, setHex] = useState("#000000");
+  const color = tinycolor(hexCode);
+  const [hsl, setHsl] = useState(color.toHsl());
+  const [hsv, setHsv] = useState(color.toHsv());
 
-  useEffect(() => {
-    // debugger;
-    const color = tinycolor(hexCode);
-    setHsl(color.toHsl());
-    setHsv(color.toHsv());
-  }, []);
-
-  useEffect(() => {
-    // debugger;
-    if (JSON.stringify(prevHsl) !== JSON.stringify(hsl)) {
-      setPrevHsv(hsv);
-      const color = tinycolor(hsl);
-      setHex(color.toHex());
-      setHsv(color.toHsv());
-    }
-  }, [hsl]);
-
-  useEffect(() => {
-    // debugger;
-    if (JSON.stringify(prevHsv) !== JSON.stringify(hsv)) {
-      const color = tinycolor(hsv);
-      setHex(color.toHex());
-      setHsl(color.toHsl());
-    }
-  }, [hsv]);
-
-  const handleHueChange = (newHsl) => {
-    console.log(`hee: ${newHsl}`);
-
-    setPrevHsl(hsl);
-    setHsl(newHsl);
+  const handleHueChange = (hsl) => {
+    setHsl(hsl);
+    const color = tinycolor(hsl);
+    onChange(color.toHex());
   };
 
-  const handleSaturationChange = (newHsv) => {
-    const color = tinycolor(newHsv);
-    const colorhsv = color.toHsv();
-    console.dir(`hsv: ${colorhsv}`);
-    debugger;
-    setPrevHsv(hsv);
-    setHsv(newHsv);
-  };
-
-  const handleColorChange = () => {
-    console.log(`Сохр: ${hex}`);
-
-    onChange(`#${hex}`);
+  const handleSaturationChange = (hsv) => {
+    setHsv(hsv);
+    const color = tinycolor(hsv);
+    onChange(color.toHex());
   };
 
   return (
@@ -94,8 +36,7 @@ const Main = ({ hexCode, onChange }) => {
           hsl={hsl}
           hsv={hsv}
           pointer={CustomPointer}
-          // onChange={handleSaturationChange}
-          onChange={() => console.log("")}
+          onChange={handleSaturationChange}
         />
       </div>
       <div className="hue">
@@ -103,8 +44,7 @@ const Main = ({ hexCode, onChange }) => {
           className="hue-vertical"
           hsl={hsl}
           pointer={CustomSlider}
-          // onChange={handleHueChange}
-          onChange={() => console.log("")}
+          onChange={handleHueChange}
           direction={"vertical"}
         />
       </div>
