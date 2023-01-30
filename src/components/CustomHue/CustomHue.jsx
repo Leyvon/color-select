d0e52faac8c2970abc05f7d6e790de942f49dd3c
Leyvon <semen_amelchenko@mail.ru>
2023-01-30 13:19:44 +0700

    refactor: move CustomSlider here from Main.jsx

--- src/components/CustomHue/CustomHue.jsx
+++ src/components/CustomHue/CustomHue.jsx
@@ -1,13 +1,17 @@
 import { CustomPicker } from "react-color";
-import "./Main/Main.css";
+import "./CustomHue.css";
 const { Hue } = require("react-color/lib/components/common");
+
+const CustomSlider = () => {
+  return <div className="slider" />;
+};
 
 const CustomHue = ({ ...props }) => {
   return (
     <div className="hue">
-      <Hue className="hue-vertical" {...props} />
+      <Hue className="hue-vertical" pointer={CustomSlider} {...props} />
     </div>
   );
 };
 
-export default CustomPicker(CustomHue);
+export default CustomHue;
