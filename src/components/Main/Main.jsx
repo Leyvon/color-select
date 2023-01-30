01058e2e2e8e71b57a12ca320c2de3d46f482268
Leyvon <semen_amelchenko@mail.ru>
2023-01-30 13:48:33 +0700

    refactor: remove unnecessary

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,76 +1,41 @@
 import React, { useState } from "react";
 import "./Main.css";
-import CustomSaturation from "../CustomSaturation";
-import CustomHue from "../CustomHue";
+import CustomSaturation from "../CustomSaturation/CustomSaturation";
+import CustomHue from "../CustomHue/CustomHue";
 
 const tinycolor = require("tinycolor2");
-// const { Saturation, Hue } = require("react-color/lib/components/common");
-
-const CustomSlider = () => {
-  return <div className="slider" />;
-};
-
-const CustomPointer = () => {
-  return <div className="pointer" />;
-};
 
 const Main = ({ color: hex, onChange }) => {
   const color = tinycolor(hex);
   const [hsl, setHsl] = useState(color.toHsl());
   const [hsv, setHsv] = useState(color.toHsv());
 
-  // const handleHueChange = (hsl) => {
-  //   setHsl(hsl);
-  //   const color = tinycolor(hsl);
-  //   onChange(color.toHex());
+  const handleHueChange = (hsl) => {
+    setHsl(hsl);
+    const color = tinycolor(hsl);
+    onChange(color.toHex());
+  };
+  const handleSaturationChange = (hsv) => {
+    setHsv(hsv);
+    const color = tinycolor(hsv);
+    onChange(color.toHex());
+  };
+
+  // const handleHueChange = (color) => {
+  //   setHsl(color.hsl);
+  //   onChange(color.hex.slice(1));
   // };
 
-  const handleHueChange = (color) => {
-    setHsl(color.hsl);
-    onChange(color.hex.slice(1));
-  };
-
-  const handleSaturationChange = (color) => {
-    console.log(color.hsv);
-    console.log(hsv);
-    setHsv(color.hsv);
-    onChange(color.hex.slice(1));
-  };
+  // const handleSaturationChange = (color) => {
+  //   setHsv(color.hsv);
+  //   onChange(color.hex.slice(1));
+  // };
 
   return (
     <div className="main-container">
-      {/* <div className="saturation">
-        <Saturation
-          hsl={hsl}
-          hsv={hsv}
-          pointer={CustomPointer}
-          onChange={handleSaturationChange}
-        />
-      </div> */}
+      <CustomSaturation hsl={hsl} hsv={hsv} onChange={handleSaturationChange} />
 
-      <CustomSaturation
-        hsl={hsl}
-        hsv={hsv}
-        pointer={CustomPointer}
-        onChange={handleSaturationChange}
-      />
-
-      {/* <div className="hue">
-        <Hue
-          className="hue-vertical"
-          hsl={hsl}
-          pointer={CustomSlider}
-          onChange={handleHueChange}
-          direction={"vertical"}
-        />
-      </div> */}
-
-      <CustomHue
-        hsl={hsl}
-        pointer={CustomSlider}
-        onChange={handleHueChange}
-        direction={"vertical"}
-      />
+      <CustomHue hsl={hsl} onChange={handleHueChange} direction={"vertical"} />
     </div>
   );
 };
